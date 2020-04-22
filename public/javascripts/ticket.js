e8c9bc31be8563c8085684977a440f6c975e8b36
nitishbhaskar <nitish27@gmail.com>
2020-04-22 10:56:25 -0500

    Converted tickets to images. Added IDs to tickets. Added footer.

--- public/javascripts/ticket.js
+++ public/javascripts/ticket.js
@@ -1,4 +1,4 @@
-const ticketTemplate = "<table class=\"table table-bordered ticketBorder table-sm\"><tr id={{row0}}></tr><tr id={{row1}}></tr><tr id={{row2}}></tr></table>";
+const ticketTemplate = "<div class=\"ticketId\">{{uniqueTicketNumber}}</div><table class=\"table table-bordered ticketBorder table-sm\"><tr id={{row0}}></tr><tr id={{row1}}></tr><tr id={{row2}}></tr></table>";
 
 $("#getTicketBtn").click(function () {
     const numberOfTickets = document.getElementById('numberOfTickets').value;
@@ -14,7 +14,7 @@ function getTicket(numberOfTickets) {
 
 function displayTickets(tickets) {
     tickets.forEach((ticket, index) => {
-        const ticketIds = { "row0": "row0" + index, "row1": "row1" + index, "row2": "row2" + index };
+        const ticketIds = { "row0": "row0" + index, "row1": "row1" + index, "row2": "row2" + index, "uniqueTicketNumber": generateUniqueId() };
         const renderedTable = Mustache.render(ticketTemplate, ticketIds);
         $("#ticket").append(renderedTable);
 
@@ -29,17 +29,33 @@ function displayTickets(tickets) {
             });
         });
     });
-
-
-}
-$("#previewBtn").click(function () {
     getImage();
-});
+}
 
+//Turns the generated ticket table into an image for easy copying
 function getImage() {
-    html2canvas($("#ticket"), {
-        onrendered: function (canvas) {
-            $("#previewImage").append(canvas);
-        }
+    html2canvas(document.querySelector("#ticket")).then(canvas => {
+        $("#ticket").empty();
+        $("#ticketAlert").show();
+        $("#ticket").append(canvas);
     });
+}
+
+//Generates unique value for the table
+function generateUniqueId() {
+    var length = 5,
+        charset = "abcdefghijklmnopqrstuvwxyz0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+
+
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
 }
