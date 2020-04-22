6b58c8284c48a07727c9ef5308aa6735284e6b47
nitishbhaskar <nitish27@gmail.com>
2020-04-22 17:12:13 -0500

    Updated ticket page layout

--- public/javascripts/ticket.js
+++ public/javascripts/ticket.js
@@ -52,10 +52,16 @@ function generateUniqueId() {
     return retVal;
 }
 
-
 function isNumberKey(evt) {
     var charCode = (evt.which) ? evt.which : evt.keyCode
     if (charCode > 31 && (charCode < 48 || charCode > 57))
         return false;
     return true;
 }
+
+$('#numberOfTickets').keyup(function () {
+    if ($(this).val() > 100) {
+        alert("No numbers above 100");
+        $(this).val('100');
+    }
+});
